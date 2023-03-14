3a801239c13aefe82e9bfff9aa72999f0462c51e
dzaki34 <122796845+dzaki34@users.noreply.github.com>
2023-03-14 10:46:46 +0700

    Add files via upload

---
+++ admin/navbar.php
@@ -0,0 +1,43 @@
+<head>
+    <link href="../css/nav.css?version=<?= filemtime("../css/nav.css")?>" rel="stylesheet">
+</head>
+<body>
+
+    <nav>
+        <div class="nav-main">
+            <div class="nav-header">
+                <img src="../assets/images/user1.png" alt="kasir">
+                <ul>
+                <li>admin</lI>
+            </ui>
+            </div>
+           
+            <ul class="menu">
+<li> <a href="index.php" class="a">
+<ion-icon class="icon" name="dashboard"></ion-icon>Dashboard
+</a></li>
+<li> <a href="outlet.php" class="a">
+<ion-icon class="icon" name="outlet"></ion-icon>Outlet
+</a></li>
+<li> <a href="paket.php" class="a">
+<ion-icon class="icon" name="paket"></ion-icon>Paket
+</a></li>
+<li> <a href="pelanggan.php" class="a">
+<ion-icon class="icon" name="pelanggan"></ion-icon>Pelanggan
+</a></li>
+<li> <a href="transaksi.php" class="a">
+<ion-icon class="icon" name="transaksi"></ion-icon>Transaksi
+</a></li>
+<li> <a href="laporan.php" class="a">
+<ion-icon class="icon" name="pengguna"></ion-icon>Laporan
+</a></li>
+<li><a href="logout.php" class="a"><span>
+<ion-icon class="icon" name="exit"></ion-icon>logout
+</span></i></a>
+                
+            </ul>
+        </div>
+
+      
+    </nav>
+</body>
